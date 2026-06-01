62bb00d150c20601015643b7407f001b32378375
PERSONALUSE\Mark Kenneth <prankstar074@gmail.com>
2026-06-01 16:41:26 +0800

    css works, red background is visible. fixed the import paths in page.js to be relative instead of absolute. now the app should work without errors.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Navbar from '@/components/Navbar';
-import About from '@/components/About';
-import Skills from '@/components/Skills';
-import Projects from '@/components/Projects';
-import Contact from '@/components/Contact';
-import Footer from '@/components/Footer';
+import Navbar from '../components/Navbar';
+import About from '../components/About';
+import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 export default function Home() {
 
