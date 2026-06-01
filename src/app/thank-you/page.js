552170a2dcefa9431fca613f0c4c38208aec0938
PERSONALUSE\Mark Kenneth <prankstar074@gmail.com>
2026-06-01 20:22:25 +0800

    added modifications for thank you page

--- src/app/thank-you/page.js
+++ src/app/thank-you/page.js
@@ -1,22 +1,26 @@
 export default function ThankYouPage() {
+  return (
+    <main className="thank-you-page">
+      <div className="thank-you-card">
+        <img
+          src="https://media.tenor.com/bMo9DEDirN0AAAAM/thank-you-thank-you-bro.gif"
+          alt="Thank You"
+          className="thank-you-image"
+        />
 
-    return (
-        <div className="thank-you-page">
+        <p className="thank-you-kicker">Message Sent</p>
 
-            <img
-                src="https://media.tenor.com/bMo9DEDirN0AAAAM/thank-you-thank-you-bro.gif"
-                alt="Thank You"
-                className="thank-you-image"
-            />
+        <h1>Thank You!</h1>
 
-            <h1>Thank You!</h1>
+        <p className="thank-you-text">
+          Your message has been submitted successfully. I appreciate you reaching
+          out and will get back to you as soon as possible.
+        </p>
 
-            <p>Your message has been submitted successfully.</p>
-
-            <a href="/" className="back-home-btn">
-                Back to Home
-            </a>
-
-        </div>
-    );
+        <a href="/" className="back-home-btn">
+          Back to Home
+        </a>
+      </div>
+    </main>
+  );
 }
